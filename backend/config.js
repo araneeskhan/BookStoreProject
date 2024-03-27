9a9b2515eda5ef2675adde22fc9f527312068264
Anees ur Rehman <105802405+araneeskhan@users.noreply.github.com>
2024-03-27 15:06:40 +0500

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,6 @@
 export const PORT = 5555;
 
-export const mongoDBURL =
-  'export const mongoDBURL = "mongodb://localhost:27017/books-collection"';
+export const mongoDBURL = "mongodb://localhost:27017/books-collection";
 
 // Please create a free database for yourself.
 // This database will be deleted after tutorial
