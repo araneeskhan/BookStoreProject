c7e50736a29867ecdda0802970f24b98a1d23030
Anees Ur Rehman <aneesurrehman1358@gmail.com>
2025-03-21 08:55:19 +0500

    Final Commit - 21st March 2025

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -26,8 +26,14 @@ export default {
           text: '#ffffff'
         }
       },
+      keyframes: {
+        'scale-up': {
+          '0%': { transform: 'scale(0.95)', opacity: 0 },
+          '100%': { transform: 'scale(1)', opacity: 1 },
+        }
+      },
       animation: {
-        'spin-slow': 'spin 3s linear infinite',
+        'scale-up': 'scale-up 0.2s ease-out',
       }
     },
   },
